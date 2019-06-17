5a09a54188f0246090d61eb66d4366f1dd2a9b46
Emily Lagattolla <elagattolla@gmail.com>
2019-06-17 16:04:53 -0500

    Updated the nav text

--- js/index.js
+++ js/index.js
@@ -47,4 +47,14 @@ let ctaImage = document.getElementById("cta-img");
 ctaImage.setAttribute('src', siteContent["cta"]["img-src"]);
 
 let middleImage = document.getElementById("middle-img");
-middleImage.setAttribute('src', 'img/mid-page-accent.jpg');
+middleImage.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+// Update the nav
+
+let navBar = document.querySelector('nav').children;
+navBar[0].textContent = siteContent['nav']['nav-item-1'];
+navBar[1].textContent = siteContent['nav']['nav-item-2'];
+navBar[2].textContent = siteContent['nav']['nav-item-3'];
+navBar[3].textContent = siteContent['nav']['nav-item-4'];
+navBar[4].textContent = siteContent['nav']['nav-item-5'];
+navBar[5].textContent = siteContent['nav']['nav-item-6'];
